0f5dd8f1e4aa96bdc89bc850bb29d0feeca33d28
Roman <Leo_dialog@mail.ru>
2021-03-02 11:58:18 +0300

    added const

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -6,10 +6,6 @@ import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
 
-    public static long d;
-    public static int testField;
-    public static String ss;
-
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
@@ -19,5 +15,4 @@ public class TimeUtil {
     public static String toString(LocalDateTime ldt){
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
-
 }
